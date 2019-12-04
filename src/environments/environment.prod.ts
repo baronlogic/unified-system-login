b75d2dac84a88194a34a3eb30b5074b6c4f378b8
Rafael Baron <baronrafael@hotmail.com>
2019-12-04 07:22:44 -0400

    using stagingapi for tests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.shocklogic.com/v1.0/'
+  apiUrl: 'https://stagingapi.shocklogic.com/v1.0/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.shocklogic.com/v1.0/'
+  apiUrl: 'https://stagingapi.shocklogic.com/v1.0/'
 };
 
 /*
