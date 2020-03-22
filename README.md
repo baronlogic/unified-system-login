958a892f50cbb38099c18ecf5ae0dfcf5567e47f
Rafael Baron <baronrafael@hotmail.com>
2020-03-22 15:01:46 -0400

    updating to angular 9

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UnifiedSystemLogin
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
 ## Development server
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,16 @@ import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'main', loadChildren: './main/main.module#MainModule' },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthModule', canActivate: [LoginGuard] },
-  { path: 'shocklogic', loadChildren: './shocklogic/shocklogic.module#ShocklogicModule', canActivate: [AuthGuard] }
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [LoginGuard]
+  },
+  {
+    path: 'shocklogic',
+    loadChildren: () => import('./shocklogic/shocklogic.module').then(m => m.ShocklogicModule),
+    canActivate: [AuthGuard]
+  },
 ];
 
 @NgModule({
